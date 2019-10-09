2fe29f8c1c95a90d536e8483266165154a6f06ce
James Devereux <devereuxjj@gmail.com>
2019-10-09 11:16:11 +0900

    database seeded

---
+++ db/migrate/20191009021040_create_kanjis.rb
@@ -0,0 +1,30 @@
+class CreateKanjis < ActiveRecord::Migration[5.2]
+  def change
+    create_table :kanjis do |t|
+      t.string :character, null: false
+      t.text :koohii1
+      t.text :koohii2
+      t.string :onyomi
+      t.string :kunyomi
+      t.string :nanori
+      t.string :english
+      t.text :examples
+      t.integer :jlpt
+      t.text :components
+      t.integer :kanji_strokes
+      t.string :kanji_radical
+      t.integer :radical_number
+      t.string :radical_reading
+      t.string :classification
+      t.string :keyword
+      t.string :grade
+      t.string :frequency
+      t.string :type
+      t.integer :jouyou
+      t.string :traditional
+      t.integer :rtk
+      t.timestamps
+
+    end
+  end
+end
